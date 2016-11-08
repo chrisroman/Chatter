9268568b6d1f499fabaeccf8409707d067d9cd13
chrisroman <cr469@cornell.edu>
2016-11-07 23:50:00 -0500

    Updated login screen UI. Login currently non-functional.

--- app/src/main/java/edu/cornell/chatter/SignInActivity.java
+++ app/src/main/java/edu/cornell/chatter/SignInActivity.java
@@ -2,12 +2,18 @@ package edu.cornell.chatter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
+
+    private static String TAG = "SignInActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView appName = (TextView) findViewById(R.id.login_app_name);
+
     }
 }
